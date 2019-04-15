31a34f54588fd04a22c1818e208caa665944fc66
ketu <liuwenjie123007@qq.com>
2019-04-15 10:12:01 +0900

    3

---
+++ src/com/msr/hospital/dao/impl/DrugDaoImpl.java
@@ -0,0 +1,28 @@
+package com.msr.hospital.dao.impl;
+
+import java.util.List;
+
+import com.msr.hospital.bean.Drug;
+import com.msr.hospital.dao.DrugDao;
+
+public class DrugDaoImpl implements DrugDao {
+
+	@Override
+	public List<Drug> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Drug findByDrid(String drid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Drug> findbyDrname(String drname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/msr/hospital/servlet/DrugSevlet.java
@@ -0,0 +1,16 @@
+package com.msr.hospital.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DrugSevlet
+ */
+@WebServlet("/DrugSevlet")
+public class DrugSevlet extends BaseServlet {
+	
+	
+}
