bd73eff13d59b4af670c2c9ababa7b2033842b40
@wendusu601 <898135716@qq.com>
2019-04-16 15:38:32 +0900

    药品信息查询做完了

--- src/com/msr/hospital/servlet/DrugSevlet.java
+++ src/com/msr/hospital/servlet/DrugSevlet.java
@@ -1,10 +1,17 @@
 package com.msr.hospital.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.msr.hospital.bean.Drug;
+import com.msr.hospital.dao.DrugDao;
+import com.msr.hospital.dao.impl.DrugDaoImpl;
 
 /**
  * Servlet implementation class DrugSevlet
@@ -12,5 +19,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/DrugSevlet")
 public class DrugSevlet extends BaseServlet {
 	
-	
+	public String drugfindAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String drid = req.getParameter("drid");
+		String drname = req.getParameter("drname");
+		List<Drug> drList = new ArrayList<Drug>();
+		
+		DrugDao drugDao = new DrugDaoImpl();
+		System.out.println("=============DrugSevlet=================");
+		
+		if(!"".equals(drid) && drid!=null) {
+			Drug drug = drugDao.findByDrid(drid);
+			drList.add(drug);
+		}
+		
+		
+		if(!"".equals(drname) && drname!=null ) {
+			drList = drugDao.findbyDrname(drname);
+		}
+		
+		
+		if("".equals(drid) && "".equals(drname)) {
+			drList =  drugDao.findAll();
+		}
+		req.getSession().setAttribute("drList", drList);
+		
+		return "/html/Guahao/medicalInfo2.jsp"; 
+	}
 }
